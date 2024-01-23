96f00898e1dbe9c3fdf3982efce5b08bfcc6f425
Simon Di Tomasso <simon.ditomasso@mail.mcgill.ca>
2024-01-23 17:04:12 -0500

    Working parallel matrix multiplication

--- src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
+++ src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
@@ -2,6 +2,8 @@ package ca.mcgill.ecse420.a1;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 public class MatrixMultiplication {
 	
@@ -17,10 +19,10 @@ public class MatrixMultiplication {
 		sequentialMultiplyMatrix(a, b);
 		long endTime = System.currentTimeMillis();
 		System.out.println("sequentialMultiplyMatrix run time: " + (endTime - startTime) + " ms");
-		//startTime = System.currentTimeMillis();
-		//parallelMultiplyMatrix(a, b);	
-		//endTime = System.currentTimeMillis();
-		//System.out.println("parallelMultiplyMatrix run time: " + (endTime - startTime) + " ms");
+		startTime = System.currentTimeMillis();
+		parallelMultiplyMatrix(a, b);	
+		endTime = System.currentTimeMillis();
+		System.out.println("parallelMultiplyMatrix run time: " + (endTime - startTime) + " ms");
 	}
 	
 	/**
@@ -56,11 +58,52 @@ public class MatrixMultiplication {
 	 * */
         public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
 			double[][] resultMatrix = new double[MATRIX_SIZE][MATRIX_SIZE];
-			
+			RecursiveAction mainTask = new MatrixMultiplyParallel(a,b, resultMatrix,0, MATRIX_SIZE);
+    		ForkJoinPool pool = new ForkJoinPool(NUMBER_THREADS);    // Create a ForkJoinPool with specified # of processors.
+			//ForkJoinPool pool = new ForkJoinPool();  // Create a ForkJoinPool with all available processors.
+    		pool.invoke(mainTask);
+  
 			return resultMatrix;
 		
 	}
-        
+    private static class MatrixMultiplyParallel extends RecursiveAction {
+		private static final int THRESHOLD = 100;
+
+    private double[][] a;
+    private double[][] b;
+    private double[][] resultMatrix;
+    private int startRow;
+    private int endRow;
+
+    MatrixMultiplyParallel(double[][] a, double[][] b, double[][] resultMatrix, int startRow, int endRow) {
+        this.a = a;
+        this.b = b;
+        this.resultMatrix = resultMatrix;
+        this.startRow = startRow;
+        this.endRow = endRow;
+    }
+
+    @Override
+    protected void compute() {
+        if (endRow - startRow <= THRESHOLD) {
+            // Directly compute if the task is small enough
+            for (int i = startRow; i < endRow; i++) {
+                for (int j = 0; j < MATRIX_SIZE; j++) {
+                    resultMatrix[i][j] = 0;
+                    for (int k = 0; k < MATRIX_SIZE; k++) {
+                        resultMatrix[i][j] += a[i][k] * b[k][j];
+                    }
+                }
+            }
+        } else {
+            // Split the task into two subtasks
+            int midRow = (startRow + endRow) / 2;
+            MatrixMultiplyParallel subTask1 = new MatrixMultiplyParallel(a, b, resultMatrix, startRow, midRow);
+            MatrixMultiplyParallel subTask2 = new MatrixMultiplyParallel(a, b, resultMatrix, midRow, endRow);
+            invokeAll(subTask1, subTask2);
+        }
+    }
+	}
         /**
          * Populates a matrix of given size with randomly generated integers between 0-10.
          * @param numRows number of rows
